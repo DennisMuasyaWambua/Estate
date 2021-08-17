87d267f12c9d8ce1b4685e791dd7bcb6e1d17cb3
terrence-brian <terrence.oyoo@strathmore.edu>
2021-08-17 21:48:16 +0300

    Tenant Template

---
+++ resources/views/user/userTemplate.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Tenant dashboard</title>
+    <!-- Bootstrap CDN'S -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+    <!-- material icons -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <!-- local css -->
+    <link href="{{asset('css/caretakerDashboard.css')}}" rel="stylesheet" >
+
+    <!-- local javascript -->
+    <script src="{{asset('js/navigationBar.js')}}"></script>
+    <!-- jquery -->
+   
+    <!-- sweet alert 2 -->
+    
+    <link rel="stylesheet" href="{{ asset('datatable/css/dataTables.bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('datatable/css/dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{asset('sweetalert2/sweetalert2.min.css')}}">
+    <link rel="stylesheet" href="{{asset('toastr/toastr.min.css')}}">
+    
+</head>
+<body style="background-image:url('/images/dine.jpg');background-size:cover;background-repeat:no-repeat;background-position:center;">
+    <div class="container">
+        <main >
+            @yield('content')
+            @section('header')
+        </main>
+    </div>
+    <script src="{{asset('datatable/js/jquery.dataTables.min.js')}}"></script>
+    <script src="{{asset('datatable/js/dataTables.bootstrap4.min.js')}}"></script>
+    <script src="{{asset('sweetalert2/sweetalert2.min.js')}}"></script>
+    <script src="{{ asset('toastr/toastr.min.js') }}"></script>
+    <script src="{{asset('js/jquery-3.6.0.min.js')}}"></script>
+    
+  
+</body>
+</html>
