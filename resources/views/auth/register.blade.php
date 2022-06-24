ff555e523bc0204cf905a650c41a3dc6f1d771e4
whateverittakes123Dennismuasyawambua <muasyathegreat4@gmail.com>
2022-06-25 01:24:15 +0300

    register page made to be iphone responsive

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,15 +2,16 @@
     @section('content')
         <form id="registration-form-main" class="form-floating" method="POST" action="{{route('register')}}">
             @csrf
-            <img src="{{asset('images/smart1.jpeg')}}" style="border-radius: 150px; width:100px;height:100px; margin-left:43%" >
-            <div class="login-title">
+</br>
+            <img id="smart-image-logo" src="{{asset('images/smart1.jpeg')}}" style="border-radius: 150px; width:100px;height:100px; margin-left:43%" >
+            <div id="login-title"class="login-title">
                 <p>SMART ESTATE</p>
                 <p>Create your account</p>
             </div>
                     
             <div class="form-group ">
                 <label class="form-control" for="name"  id="name" >Name</label>
-                <input name="name" type="text" class="form-control is-invalid" placeholder="Name"  autofocus> 
+                <input id = "name-input"name="name" type="text" class="form-control is-invalid" placeholder="Name"  autofocus> 
                 @error('name')
                     <span class="invalid-feedback is-invalid" role="alert">
                         <strong>{{ $message }}</strong>
@@ -21,7 +22,7 @@
 
             <div class="form-group">
                 <label class="form-control" for="email"  id="email" >Email</label>
-                <input name="email" type="email" class="form-control is-invalid" placeholder="Email"> 
+                <input id="email-input" name="email" type="email" class="form-control is-invalid" placeholder="Email"> 
                 @error('email')
                     <span class="invalid-feedback is-invalid" role="alert">
                         <strong>{{ $message }}</strong>
@@ -31,7 +32,7 @@
 
             <div class="form-group">
                 <label class="form-control" for="password"  id="password" >Password</label>
-                <input name="password" type="password" class="form-control is-invalid" placeholder="Password" >
+                <input id=password-input name="password" type="password" class="form-control is-invalid" placeholder="Password" >
                 @error('password')
                     <span class="invalid-feedback is-invalid" role="alert">
                         <strong>{{ $message }}</strong>
@@ -42,7 +43,7 @@
 
             <div class="form-group">
                 <label class="form-control" for="password"  id="password-confirm" >Confirm Password</label>
-                <input name="password_confirmation" type="password" class="form-control is-invalid" placeholder="Confirm Password"  required autocomplete="new-password">
+                <input id="confirmation-input" name="password_confirmation" type="password" class="form-control is-invalid" placeholder="Confirm Password"  required autocomplete="new-password">
                 @error('password-confirm')
                     <span class="invalid-feedback is-invalid" role="alert">
                         <strong>{{ $message }}</strong>
@@ -57,10 +58,16 @@
                     </select>
                 </label>
             </div>
-            <input name="register" id="register" class="btn btn-block login-btn mb-4" type="submit" value="Register">
+                </br>
+            <input name="register" id="register" class="btn-primary" type="submit" value="Register">
             <div class="redirection">
-                <a href="#!" class="forgot-password-link">Forgot password?</a>
-                <small><p class="login-card-footer-text">Alredy have an account? <a href="{{ route('login') }}" class="text-reset"id="register-login">Login here</a></p></small>
+                </br>
+                <small>
+                    <p id="login-card-footer-text" class="login-card-footer-text">
+                         <a href="{{ route('login') }}" class="text-reset"id="register-login">Login here</a></br>
+                         <a href="#!"id ="forgot-password-link" class="forgot-password-link">Forgot password?</a>
+                    </p>
+                </small>
             </div>
             
         </form>
